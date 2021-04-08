09ee8e02721e7161c6974e588337786e6578fd7d
robertsm21 <56083568+robertsm21@users.noreply.github.com>
2021-04-07 21:50:13 -0400

    Update turnFunctions.py
    new USFS left turn

--- Capstone Data Gathering/turnFunctions.py
+++ Capstone Data Gathering/turnFunctions.py
@@ -1,6 +1,21 @@
 import time
 import board
 from adafruit_motorkit import MotorKit
+from usfs import USFS_Master
+import math
+
+MAG_RATE = 100 #Hz
+ACCEL_RATE = 200 #Hz
+GYRO_RATE = 200 #Hz
+BARO_RATE = 50 #Hz
+Q_RATE_DIVISOR = 3 #1/3 gyro rate
+
+usfs = USFS_Master(MAG_RATE, ACCEL_RATE, GYRO_RATE, BARO_RATE, Q_RATE_DIVISOR)
+
+#Start the USFS in master mode
+if not usfs.begin():
+    print(usfs.getErrorString())
+    exit(1)
 
 wheels = MotorKit(i2c = board.I2C())
 
@@ -26,6 +41,45 @@ class robotManuevers:
         wheels.motor3.throttle = 0
         wheels.motor4.throttle = 0
         time.sleep(.5)
+        
+    def turnLeft90USFS():
+        wheels.motor3.throttle = 0
+        wheels.motor4.throttle = 0
+        time.sleep(.5)
+        for count in range(1000)
+            usfs.checkEventStatus()
+        
+            if usfs.gotError():
+                print('ERROR: ' + usfs.getErrorString())
+                exit(1)
+            if (usfs.gotQuaternion()):
+                qw, qx, qy, qz = usfs.readQuaternion()
+                angle = math.atan2(2.0*(qx*qy+qw*qz), qw*qw+qx*qx-qy*qy-qz*qz)
+                angle*=180.0
+                angle+=9.1
+                if angle < 0: angle += 360.0
+                startAngle = angle
+        print('Start angle: ', startAngle)
+        time.sleep(.05)
+        while abs(startAngle - angle) < 90:
+            usfs.checkEventStatus()
+            if usfs.gotError():
+                print("ERROR: " + usfs.getErrorString())
+            if (usfs.gotQuaternion()):
+                qw, qx, qy, qz = usfs.readQuaternion()
+                angle = math.atan2(2.0*(qx*qy+qw*qz), qw*qw+qx*qx-qy*qy-qz*qz)
+                angle*=180.0
+                angle+=9.1
+                if angle < 0: angle += 360.0
+                print("current angle: ", angle)
+                print("start angle: ", startAngle)
+                print("difference: ", abs(startAngle - angle))
+                wheels.motor3.throttle = 0.7
+                wheels.motor4.throttle = -0.65
+        wheels.motor3.throttle = 0
+        wheels.motor4.throttle = 0
+        time.sleep(.5)
+        
         
     def turnRight90():
         wheels.motor3.throttle = 0
